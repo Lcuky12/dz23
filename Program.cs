541a5aca03d341367dab0df4b6a4fe446ebe912b
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 13:30:33 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string stopWord = "exit";
+            bool canExamination = true;
+
+            while (canExamination) 
+            {
+                Console.WriteLine("Введите сообщение");
+                userInput= Console.ReadLine();
+
+                if (userInput == stopWord)
+                {
+                   canExamination= false;
+                }
+            }
+        }
+    }
+}
